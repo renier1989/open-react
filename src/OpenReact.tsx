f3542d061c3394ca1491d4479f1e56a7606e39a2
Renier Vargas <renier1989@gmail.com>
2024-05-23 06:42:17 -0500

    creando la estructura del proyecto para el frontend

---
+++ src/OpenReact.tsx
@@ -0,0 +1,5 @@
+export const OpenReact = () => {
+  return (
+    <div>OpenReact</div>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import { OpenReact } from './OpenReact'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <OpenReact/>
+  </React.StrictMode>,
+)
