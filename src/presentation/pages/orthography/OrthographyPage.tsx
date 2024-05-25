99bcd2089b6904b43ea4703bdd5eb79d43817962
Renier Vargas <renier1989@gmail.com>
2024-05-25 07:19:51 -0500

    Creando un indicador de escritura

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,4 +1,4 @@
-import { GptMessage, MyMessage } from "../../components"
+import { GptMessage, MyMessage, TypingLoader } from "../../components"
 
 export const OrthographyPage = () => {
   return (
@@ -10,7 +10,7 @@ export const OrthographyPage = () => {
               {/* aqui muestro un mensaje de bienvenida o presentacacion para esta funcionaldiad */}
               <GptMessage text="Puedes escribir texto aqui y te ayudo con la ortografia. "/>
               <MyMessage text="Hola OpenIA "/>
-
+              <TypingLoader className="fade-in"/>
           </div>
         </div>
       </div>
