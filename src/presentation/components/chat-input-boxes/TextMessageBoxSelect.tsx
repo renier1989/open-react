775a0cbeec76545fff5907be951a5beef5d06a32
Renier Vargas <renier1989@gmail.com>
2024-06-15 11:29:43 -0500

    Preparando el UI para las traducciones y seleccion de idiomas

--- src/presentation/components/chat-input-boxes/TextMessageBoxSelect.tsx
+++ src/presentation/components/chat-input-boxes/TextMessageBoxSelect.tsx
@@ -18,6 +18,7 @@ export const TextMessageBoxSelect = ({ onSendMessage, placeholder, disabledCorre
   const handleSendMessage = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if(message.trim().length === 0) return;
+    if(selectedOption === '') return;
     onSendMessage(message,selectedOption);
     setMessage('');
   };
--- src/presentation/pages/translate/TranslatePage.tsx
+++ src/presentation/pages/translate/TranslatePage.tsx
@@ -1,5 +1,69 @@
+import { useState } from "react";
+import { GptMessage, MyMessage, TypingLoader, TextMessageBoxSelect } from "../../components";
+
+interface Message {
+    text: string;
+    isGpt: boolean;
+}
+
+const languages = [
+  { id: "alemán", text: "Alemán" },
+  { id: "árabe", text: "Árabe" },
+  { id: "bengalí", text: "Bengalí" },
+  { id: "francés", text: "Francés" },
+  { id: "hindi", text: "Hindi" },
+  { id: "inglés", text: "Inglés" },
+  { id: "japonés", text: "Japonés" },
+  { id: "mandarín", text: "Mandarín" },
+  { id: "portugués", text: "Portugués" },
+  { id: "ruso", text: "Ruso" },
+];
+
 export const TranslatePage = () => {
-  return (
-    <div>TranslatePage</div>
-  )
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([])
+
+    const handlePost = async (text: string, selectedOption: string) => {
+        setIsLoading(true);
+        const newMessage = `Traduce el siguiente Texto: " ${text} " al idioma: ${selectedOption}`;
+        setMessages((prev) => [...prev, { text: newMessage, isGpt: false }]);
+        
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+        // TODO: añadir el mensaje de GPT aqui 
+    }
+
+    return (
+        <>
+            <div className="chat-container">
+                <div className="chat-messages">
+                    <div className="grid grid-cols-12 gap-y-2 ">
+
+                        {/* aqui muestro un mensaje de bienvenida o presentacacion para esta funcionaldiad */}
+                        <GptMessage text="¿Que texto te gustaria que tradusca para ti?" />
+
+                        {/* aqui valido si los mensajes son de GPT o son mios */}
+                        {messages.map((message, index) => (
+                            message.isGpt ?
+                                (<GptMessage key={index} text={message.text} />)
+                                :
+                                (<MyMessage key={index} text={message.text} />)
+                        ))}
+                        {isLoading && (
+                            <div className="col-start-1 col-end-12 fade-in">
+                                <TypingLoader />
+                            </div>
+                        )}
+
+                    </div>
+                </div>
+                <TextMessageBoxSelect
+                    onSendMessage={handlePost}
+                    placeholder="Escribe aqui lo que deseas traducir"
+                    options={languages}
+                />
+            </div>
+        </>
+    )
 }
