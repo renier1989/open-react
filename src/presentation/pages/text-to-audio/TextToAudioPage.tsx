c7a23a6b78c2e3c55d6d9fd55021ebdcc9187790
Renier Vargas <renier1989@gmail.com>
2024-06-17 07:32:34 -0500

    Preparacion del UI para generar texto a audio

--- src/presentation/pages/text-to-audio/TextToAudioPage.tsx
+++ src/presentation/pages/text-to-audio/TextToAudioPage.tsx
@@ -1,5 +1,69 @@
+import { useState } from "react"
+import { GptMessage, MyMessage, TypingLoader, TextMessageBoxSelect } from "../../components";
+
+interface Message {
+    text: string;
+    isGpt: boolean;
+}
+
+const disclaimer = `## Dame un texto y yo lo regresare como un audio.
+  * Nota: Todos los audios son generados por una AI.
+`;
+
+const voices = [
+  { id: "nova", text: "Nova" },
+  { id: "alloy", text: "Alloy" },
+  { id: "echo", text: "Echo" },
+  { id: "fable", text: "Fable" },
+  { id: "onyx", text: "Onyx" },
+  { id: "shimmer", text: "Shimmer" },
+]
+
 export const TextToAudioPage = () => {
-  return (
-    <div>TextToAudioPage</div>
-  )
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([])
+
+    const handlePost = async (text: string , selectedVoice:string) => {
+        setIsLoading(true);
+        // tomo los mensajes anterior y le agrego un nuevo que sera un mensaje mio (PRUEBA)
+        setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+        // TODO: aqui se llamara al useCase el caso de uso
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+        // TODO: añadir el mensaje de GPT aqui 
+    }
+
+    return (
+        <>
+            <div className="chat-container">
+                <div className="chat-messages">
+                    <div className="grid grid-cols-12 gap-y-2 ">
+
+                        {/* aqui muestro un mensaje de bienvenida o presentacacion para esta funcionaldiad */}
+                        <GptMessage text={disclaimer} />
+
+                        {/* aqui valido si los mensajes son de GPT o son mios */}
+                        {messages.map((message, index) => (
+                            message.isGpt ?
+                                (<GptMessage key={index} text="esto es de GPT." />)
+                                :
+                                (<MyMessage key={index} text={message.text} />)
+                        ))}
+                        {isLoading && (
+                            <div className="col-start-1 col-end-12 fade-in">
+                                <TypingLoader />
+                            </div>
+                        )}
+
+                    </div>
+                </div>
+                <TextMessageBoxSelect
+                    onSendMessage={handlePost}
+                    placeholder="Escribe aqui el texto"
+                    options={voices}
+                />
+            </div>
+        </>
+    )
 }
