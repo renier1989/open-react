dd69ab05685fa8fbd44bbecea9d0cc5530fdda55
Renier Vargas <renier1989@gmail.com>
2024-06-23 16:25:57 -0500

    consumiento el endpoint para genera la imagen

--- src/presentation/pages/image-generation/ImageGenerationPage.tsx
+++ src/presentation/pages/image-generation/ImageGenerationPage.tsx
@@ -1,5 +1,67 @@
+
+import { useState } from "react"
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox } from "../../components";
+import { imageGenerationUseCase } from "../../../core/use-cases";
+
+interface Message {
+    text: string;
+    isGpt: boolean;
+    info? : {
+      imageUrl: string;
+      alt: string;
+    }
+}
+
 export const ImageGenerationPage = () => {
-  return (
-    <div>ImageGenerationPage</div>
-  )
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([])
+
+    const handlePost = async (text: string) => {
+        setIsLoading(true);
+        // tomo los mensajes anterior y le agrego un nuevo que sera un mensaje mio (PRUEBA)
+        setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+        
+        // TODO: aqui se llamara al useCase el caso de uso
+        const imageInfo = await imageGenerationUseCase(text);
+        setIsLoading(false);
+
+        if(!imageInfo){
+          return setMessages((prev)=> [...prev, {text: 'No se pudo generar la imagen', isGpt: true}]);
+        }
+
+        // TODO: añadir el mensaje de GPT aqui 
+    }
+
+    return (
+        <>
+            <div className="chat-container">
+                <div className="chat-messages">
+                    <div className="grid grid-cols-12 gap-y-2 ">
+
+                        {/* aqui muestro un mensaje de bienvenida o presentacacion para esta funcionaldiad */}
+                        <GptMessage text="Dime que imagen quieres generar" />
+
+                        {/* aqui valido si los mensajes son de GPT o son mios */}
+                        {messages.map((message, index) => (
+                            message.isGpt ?
+                                (<GptMessage key={index} text="esto es de GPT." />)
+                                :
+                                (<MyMessage key={index} text={message.text} />)
+                        ))}
+                        {isLoading && (
+                            <div className="col-start-1 col-end-12 fade-in">
+                                <TypingLoader />
+                            </div>
+                        )}
+
+                    </div>
+                </div>
+                <TextMessageBox
+                    onSendMessage={handlePost}
+                    placeholder="Escribe aqui tu pregunta"
+                    disabledCorrections={false}
+                />
+            </div>
+        </>
+    )
 }
